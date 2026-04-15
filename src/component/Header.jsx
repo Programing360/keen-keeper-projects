76d6ce1cd588920ab1bd82e2fb33e75013ca1f69
Md Limon <fhlimon360@gmail.com>
2026-04-15 07:10:55 +0600

    added footer section

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,26 +5,31 @@ import { usePathname } from "next/navigation";
 import { FaHome } from "react-icons/fa";
 import { ImStatsDots } from "react-icons/im";
 import { IoTimeOutline } from "react-icons/io5";
-import logoIcon from '../../public/assets/logo.png';
+import logoIcon from "../../public/assets/logo.png";
 import Image from "next/image";
 const Header = () => {
+  const pathName = usePathname();
 
-    const pathName = usePathname();
-
-    const activeClass = (path) => {
-      return  pathName === path 
-      
-      ? " btn bg-[#244D3F] text-white" : "btn text-black";
-    }
-
+  const activeClass = (path) => {
+    return pathName === path
+      ? " btn bg-[#244D3F] text-white"
+      : "btn text-black";
+  };
 
   return (
     <div className="bg-base-100">
       <div className="navbar container mx-auto">
-        
         <div className="navbar-start ">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <Image src={logoIcon} alt="Logo" width={130} height={50} />
+        </div>
+        <div className="navbar-center hidden lg:flex "></div>
+        <div className="dropdown navbar-end">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -37,24 +42,37 @@ const Header = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
+                  d="M4 6h16M4 12h16M4 18h7"
                 />{" "}
               </svg>
             </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content gap-3 rounded-box z-1  w-52 p-2 shadow"
+            >
+              <li>
+                <Link href="/" className={activeClass("/")}>
+                  <FaHome /> Home
+                </Link>
+              </li>
+              <Link href="/timeline" className={activeClass("/timeline")}>
+                <IoTimeOutline /> Timeline
+              </Link>
+              <Link href="/stats" className={activeClass("/stats")}>
+                <ImStatsDots /> Stats
+              </Link>
+            </ul>
           </div>
-          <Image src={logoIcon} alt="Logo" width={130} height={50} />
         </div>
-        <div className="navbar-center hidden lg:flex">
-        </div>
-        <div className="navbar-end flex gap-4">
+        <div className="navbar-end hidden gap-4 md:flex">
           <Link href="/" className={activeClass("/")}>
-           <FaHome /> Home
+            <FaHome /> Home
           </Link>
           <Link href="/timeline" className={activeClass("/timeline")}>
-           <IoTimeOutline /> Timeline
+            <IoTimeOutline /> Timeline
           </Link>
           <Link href="/stats" className={activeClass("/stats")}>
-           <ImStatsDots /> Stats
+            <ImStatsDots /> Stats
           </Link>
         </div>
       </div>
